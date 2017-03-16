eafd602690fb1c1321ef73014373ed7df3228cf9
mtnview101 <mtnview101@gmail.com>
2017-03-15 23:57:55 -0700

    commit 4

--- src/core/CREATE.java
+++ src/core/CREATE.java
@@ -3,18 +3,19 @@ import java.io.*;
 public class Create {
 
 	       public static void main(String[] args) throws IOException {
-	              File f = new File("hw_9.txt");
+	              File f = new File("hw_99.txt");
 	              String content1 = "01, Alex Moore, alex.moore@gmail.com";
-
-	          String content2 = "02, Nikita Moore, nikita.moore@gmail.com";
+	              String content2 = "02, Nikita Moore, nikita.moore@gmail.com";
+	              String content3 = "03, Dmitry Nakhabtsev, dmitry.nakhabtsev@gmail.com";
+	              String content4 = "04, James Gosling, james.gosling@gmail.com";
 
 	              if (!f.exists()) {f.createNewFile();}
 	              FileWriter fw = new FileWriter(f.getAbsoluteFile(), true);      // Append
 	              BufferedWriter bw = new BufferedWriter(fw);
-	              bw.write(content1);
-	              bw.newLine();
-	              bw.write(content2);
-	              bw.newLine();
+	              bw.write(content1); bw.newLine();
+	              bw.write(content2); bw.newLine();
+	              bw.write(content3); bw.newLine();
+	              bw.write(content4); bw.newLine();
 	              bw.close();
 	       System.out.println("Done"); }}
 
